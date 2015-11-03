d91d510e2dd0550ce9aa5a96b7723f591b809f16
israbbani <rabbanii@carleton.edu>
2015-11-03 00:32:55 -0600

    Added support for geofences endpoint. Look at the slack server channel for temporary instructions on how to call the server. I'll update the readme soon.

--- node-server/server.js
+++ node-server/server.js
@@ -4,6 +4,7 @@ var utils = require('./app/services/utils');
 var server = restify.createServer();
 
 var db = mongojs('mongodb://admin:admin123@ds045694.mongolab.com:45694/carleton-sesq', ['content'], {authMechanism: 'ScramSHA1'});
+var geoDB = mongojs('mongodb://admin:admin123@ds045694.mongolab.com:45694/carleton-sesq', ['geofences'], {authMechanism: 'ScramSHA1'});
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -16,11 +17,38 @@ server.post("/landmarks", function (req, res, next) {
 	var radius = req.body.geofence.radius;
 	var center = req.body.geofence.location;
     db.content.find(function (err, content) {
-    	var output = []
+    	var output = [];
     	if (content) {
     		for (var i = 0; i < content.length; i++) {
     			if (utils.distanceTo(radius,center,content[i].location)) {
 					output.push(content[i]);
+    			}
+    		}
+	        res.writeHead(200, {
+	            'Content-Type': 'application/json; charset=utf-8'
+	        });    		
+	        res.end(JSON.stringify({content: output}));
+    	} else {
+	        res.writeHead(404, {
+	        });    		
+	        res.end(JSON.stringify(err));
+    	}
+    });
+    return next();
+});
+
+// Endpoint for Landmarks
+server.post("/geofences", function (req, res, next) {
+	// extract useful information from the request
+	var requestBody = req.body;
+	var radius = req.body.geofence.radius;
+	var center = req.body.geofence.location;
+    geoDB.geofences.find(function (err, geofences) {
+    	var output = [];
+    	if (geofences) {
+    		for (var i = 0; i < geofences.length; i++) {
+    			if (utils.distanceTo(radius,center,geofences[i].geofence.location)) {
+					output.push(geofences[i]);
     			}
     		}
 	        res.writeHead(200, {
