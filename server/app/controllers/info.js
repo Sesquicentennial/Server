3d4b79dd5064fdb6011d0d1f630bf9601018b058
israbbani <rabbanii@carleton.edu>
2015-11-20 20:16:46 -0600

    Info landmark should now be working for one geofence

--- server/app/controllers/info.js
+++ server/app/controllers/info.js
@@ -1,33 +1,47 @@
 var utils = require('../services/utils'),
     database = require('../core/database');
+    Q = require('q');
 
 var getInfo = function(req, res, next) {
+    var self = this;
     var requestBody = req.body;
-    var query = 'select * from geofences \
-                 join infoGeofence join info \
-                 where geofences.geofence_id = 1 \
-                 and geofences.geofence_id = infoGeofence.g_id \
-                 and info.info_id = infoGeofence.i_id'
-    database.connection.query(query, function(err, rows, fields) {
-        if (err) {
-            res.writeHead(404, {});
-            res.end(JSON.stringify(err));
-        } else {
-            var output = [];
-            if (rows) {
-                for (var i = 0; i < rows.length; i++) {
-                    output.push({
-                            "_id": rows[i].geofence_id,
-                            "type": rows[i].type,
-                            "summary": rows[i].summary,
-                            "data": rows[i].data,
-                    });
+    var geofences = req.body.geofences;
+    for (var i = 0; i < geofences.length; i++) {
+        // query for each geofence and get the id
+        var query = 'SELECT geofence_id from geofences where geofences.name like "' + geofences[i] + '"';
+        var deferred = Q.defer();
+        database.connection.query(query, function(err, rows, fields) {
+            if (err) throw err;
+            deferred.resolve(rows[0].geofence_id);
+        });
+        deferred.promise.then(function(id) {
+            var query = 'select * from geofences join \
+                         infoGeofence join info where \
+                         geofences.geofence_id = ' + id + ' and \
+                         geofences.geofence_id = infoGeofence.g_id \
+                         and info.info_id = infoGeofence.i_id'
+            database.connection.query(query, function(err, rows, fields) {
+                if (err) {
+                    res.writeHead(404, {});
+                    res.end(JSON.stringify(err));
+                } else {
+                    output = [];
+                    if (rows) {
+                        for (var i = 0; i < rows.length; i++) {
+                            output.push({
+                                    "_id": rows[i].geofence_id,
+                                    "type": rows[i].type,
+                                    "summary": rows[i].summary,
+                                    "data": rows[i].data,
+                            });
+                        }
+                    }
+                    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+                    res.end(JSON.stringify({ content: output }));
                 }
-            }
-            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-            res.end(JSON.stringify({ content: output }));
-        }
-    });
+            }); 
+        })
+    }
 };
 
 module.exports = {
