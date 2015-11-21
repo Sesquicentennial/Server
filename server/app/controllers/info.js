9981a1881d4c0c557e50273328f6ea2f2d024cbd
chetaldrich <chetaldrich@gmail.com>
2015-11-20 19:46:30 -0600

    Port info request to MySQL

--- server/app/controllers/info.js
+++ server/app/controllers/info.js
@@ -3,22 +3,31 @@ var utils = require('../services/utils'),
 
 var getInfo = function(req, res, next) {
     var requestBody = req.body;
-
-    database.infoDB.info.find( { geofences: { $in: req.body.geofences } },
-        function(error, info) {
-            output = [];
-            if (info) {
-                for (var i = 0; i < info.length; i++) {
-                    output.push(info[i]);
+    var query = 'select * from geofences \
+                 join infoGeofence join info \
+                 where geofences.geofence_id = 1 \
+                 and geofences.geofence_id = infoGeofence.g_id \
+                 and info.info_id = infoGeofence.i_id'
+    database.connection.query(query, function(err, rows, fields) {
+        if (err) {
+            res.writeHead(404, {});
+            res.end(JSON.stringify(err));
+        } else {
+            var output = [];
+            if (rows) {
+                for (var i = 0; i < rows.length; i++) {
+                    output.push({
+                            "_id": rows[i].geofence_id,
+                            "type": rows[i].type,
+                            "summary": rows[i].summary,
+                            "data": rows[i].data,
+                    });
                 }
-                res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-                res.end(JSON.stringify({ content: output }));   
-            } else if (error) {
-                res.writeHead(404, {});
-                res.end(JSON.stringify(error));
             }
-        });
-    return next();
+            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+            res.end(JSON.stringify({ content: output }));
+        }
+    });
 };
 
 module.exports = {
