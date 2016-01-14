bbcc91d67fa128bcdd79e3e30af7a013a24dd1b4
israbbani <rabbanii@carleton.edu>
2016-01-13 19:20:30 -0600

    Completed the Events endpoint. Returns a list
    of events from the carleton campus calendar. Added getCurrentDate() function to utils.

--- server/app/controllers/events.js
+++ server/app/controllers/events.js
@@ -1,21 +1,32 @@
-var database = require('../core/database');
+var calendar = require('../services/calendar'),
+    utils = require('../services/utils');
+
+/**
+    request from client looks like this
+    {
+       "startTime": "2012-06-22 05:40:06", 
+       "limit": 5
+    }
+**/
 
 var getEvents = function(req, res, next) {
-    var requestBody = req.body;
-    var startTime = req.body.timespan.startTime;
-    var endTime = req.body.timespan.endTime;
 
-    // just some dummy code for now to test if the endpoint is working. 
-    // need to create a database table for the data before querying. 
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-    var output = [];
-    output.push({
-            "startTime": startTime,
-            "endTime": endTime, 
-            "venue": "CMC"
-    });
+    var requestBody = req.body;
+    var startTime = req.body.startTime ? req.body.startTime : utils.getCurrentDate() ;
+    var limit = req.body.limit ? req.body.limit : 5;
     
-    res.end(JSON.stringify({ content: output }));
+    var requestUrl = 'https://apps.carleton.edu/calendar/?start_date=' + startTime + '&format=ical';
+
+    calendar.getCalendar(requestUrl,startTime)
+    .then( function(response) {
+        var response = response.splice(0,limit);
+        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+        res.end(JSON.stringify({ content: response }));
+    })
+    .catch( function(err) {
+        res.writeHead(404, JSON.stringify(err));
+        res.end(JSON.stringify(err));
+    });
 
 };
 
