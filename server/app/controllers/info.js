a8cc3c39752549abfbf09d926030f3004f4fe30b
israbbani <rabbanii@carleton.edu>
2016-01-16 18:48:16 -0600

    Refactored info endpoint for multiple geofences
    It should now respond correctly to a request for multiple geofences. Also returns the name of the geofence that the piece of information was request for.

--- server/app/controllers/info.js
+++ server/app/controllers/info.js
@@ -3,48 +3,76 @@ var utils = require('../services/utils'),
     Q = require('q');
 
 var getInfo = function(req, res, next) {
+
     var self = this;
     var requestBody = req.body;
-    var geofences = req.body.geofences;
+    var geofences = req.body.geofences ? req.body.geofences : [];
+    var outerDef = Q.defer();
+    var defs = {};
+    var promises = [];
+
+    // create query to get geofence ids
+    var query = 'SELECT geofence_id,name from geofences where name like '
+   
     for (var i = 0; i < geofences.length; i++) {
-        // query for each geofence and get the id
-        var query = 'SELECT geofence_id from geofences where geofences.name like "' + geofences[i] + '"';
-        var deferred = Q.defer();
-        database.connection.query(query, function(err, rows, fields) {
-            if (err) throw err;
-            deferred.resolve(rows[0].geofence_id);
-        });
-        deferred.promise.then(function(id) {
-            var query = 'select * from geofences join \
-                         infoGeofence join info where \
-                         geofences.geofence_id = ' + id + ' and \
-                         geofences.geofence_id = infoGeofence.g_id \
-                         and info.info_id = infoGeofence.i_id'
-            database.connection.query(query, function(err, rows, fields) {
-                if (err) {
-                    res.writeHead(404, {});
-                    res.end(JSON.stringify(err));
-                } else {
-                    output = [];
-                    if (rows) {
-                        for (var i = 0; i < rows.length; i++) {
-                            output.push({
-                                    "_id": rows[i].geofence_id,
-                                    "name": geofences[i],
-                                    "type": rows[i].type,
-                                    "summary": rows[i].summary,
-                                    "data": rows[i].data,
+        query = query + '"' + geofences[i] + '"';
+        if (i !== geofences.length - 1) query = query + ' OR name like ';
+    }
+    // request the ids from the database
+    database.connection.query(query, function(err, rows, fields) {
+        if (err) {
+            throw err;
+        } else {
+            outerDef.resolve(rows);
+            for (var i = 0; i < rows.length; i++) {
+                var geofenceName = rows[i].name;
+                var geofenceId = rows[i].geofence_id;
+                defs[geofenceId] = Q.defer();
+                promises.push(defs[geofenceId].promise);
+                // create request for each geofence
+                var query = 'select * from geofences join \
+                             infoGeofence join info where \
+                             geofences.geofence_id = ' + geofenceId + ' and \
+                             geofences.geofence_id = infoGeofence.g_id \
+                             and info.info_id = infoGeofence.i_id'
+                // get the data for each geofence
+                database.connection.query(query, function(err, rows, fields) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        if (rows) {
+                            defs[rows[0].geofence_id].resolve({
+                                    "_id": rows[0].geofence_id,
+                                    "name": rows[0].name,
+                                    "type": rows[0].type,
+                                    "summary": rows[0].summary,
+                                    "data": rows[0].data,
                             });
                         }
                     }
-                    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-                    res.end(JSON.stringify({ content: output }));
-                }
-            });
-        })
-    }
-};
+                });
+            }
+        }
+    });
 
+    outerDef.promise
+    .then(function(){
+        if (promises.length > 0) {
+            Q.all(promises).then( function(output) {
+                res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+                res.end(JSON.stringify({ content: output }));
+            });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+            res.end(JSON.stringify({content: {}}));
+        }           
+    })
+    .catch(function (err){
+        res.writeHead(404, {});
+        res.end(JSON.stringify(err));
+    });
+}       
+    
 module.exports = {
     getInfo: getInfo
 }
