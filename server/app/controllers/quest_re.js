a20dbe3e331e766cdd0048bee4eee355fff472c2
israbbani <rabbanii@carleton.edu>
2016-02-04 16:00:16 -0600

    Returns basic quest data without images, waypoints, clues and hints

--- server/app/controllers/quest_re.js
+++ server/app/controllers/quest_re.js
@@ -4,8 +4,38 @@ var utils = require('../services/utils'),
     Q = require('q');
 
 var getQuest = function (req, res, next) {
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-    res.end(JSON.stringify({ content: questObject }));
+
+	// checks to see if optional parameter limit included in request
+    var limit = req.body.limit ? req.body.limit : 20;
+
+    // this will resolve when the first callback fires (quests are returned from the DB)
+    var outerDef = Q.defer();
+
+    // JSON object that will contain all the data returned from the server
+    var output = {};
+
+    // var defs = {};
+    // var promises = [];
+
+    var questQuery = 'SELECT * FROM quests';
+
+   	database.connection.query(questQuery, function (err, rows, fields) {
+   		if (err) {
+   			throw err
+   		} else {
+   			if (rows.length > 0) {
+   				outerDef.resolve(rows);
+   			} else {
+   				outerDef.resolve([]);
+   			}
+   		}
+   	});
+
+   	outerDef.promise.then(function(response){
+	    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+	    res.end(JSON.stringify({ content: response }));   		
+   	});
+
 }
 
 module.exports = {
