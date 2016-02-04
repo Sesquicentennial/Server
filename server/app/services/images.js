6238f0562ab5754aa35715204cd317fa747232d9
israbbani <rabbanii@carleton.edu>
2016-02-04 16:55:27 -0600

    added getImages() function that allows async fetch of multiple images. returns promise that resolves to all images

--- server/app/services/images.js
+++ server/app/services/images.js
@@ -20,9 +20,9 @@ var getImage = function(args) {
 
 	var imageBase = config.images_base_path;
 	var def = Q.defer();
-	var output = {};
 	var imageFormat = args.imageFormat
 	var imagePath = imageBase + '/' + args.imageCategory + '/' + args.imageName + '.' + args.imageFormat;
+
 	fs.readFile(imagePath, function(err,data) {
 		if (err) {
 			throw err;
@@ -33,9 +33,27 @@ var getImage = function(args) {
 			});
 		}
 	});
+
+	return def.promise;
+}
+
+var getImages = function(imageList) {
+	var def = Q.defer();
+	var imagePromises = [];
+
+	for (var i = 0; i < imageList.length; i++) {
+		imagePromises.push(getImage(imageList[i]));
+	}
+
+	Q.all(imagePromises)
+	.then(function (images) {
+		def.resolve(images);
+	});
+
 	return def.promise;
 }
 
 module.exports = {
-	getImage : getImage
+	getImage : getImage,
+	getImages: getImages
 }
