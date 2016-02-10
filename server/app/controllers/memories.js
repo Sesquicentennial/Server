f8b8941a50b8ca6042c39a55fa0235aec1d5232b
israbbani <rabbanii@carleton.edu>
2016-02-10 01:03:16 -0600

    Data is fetched based on location.
    Basic reading feature based on location working now. Need to get write working. Also, need to throw in some server side processing for the images

--- server/app/controllers/memories.js
+++ server/app/controllers/memories.js
@@ -3,37 +3,44 @@ var Flickr = require("flickrapi"),
 
 var getMemories = function(req, res, next) {
 
-	if (req.body && req.body.geofence) {
-
-	}
-
-
-
 	Flickr.authenticate(config.flickrOptions, function(err, flickr) {
+		
 		if (flickr) {
 
-			console.log('Connected to Flickr');
-
-			console.log(request);
-
-			flickr.photos.geo.photosForLocation({
+			flickr.photos.search({
+				
 				api_key: config.flickrOptions.api_key,
 				user_id: config.flickrOptions.user_id,
 				authenticated: true,
 				lat: req.body.lat,
 				lon: req.body.lng,
 				accuracy: 16
+
 			}, function(err, result) {
-				console.log(result);
+
+				if (err) {
+					throw err;
+				} else if (result) {
+					console.log(result);
+				}
+
 			});
 
+		} else if (err) {
+
+			throw err
+
 		}
+
 	});
 
 
 }
 
 var addMemories = function(req, res, next) {
+
+
+
 }
 
 module.exports = {
