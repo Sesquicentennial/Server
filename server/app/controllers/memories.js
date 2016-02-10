5d96bae0d839f0578727bd1eac57aaf457c00a0d
israbbani <rabbanii@carleton.edu>
2016-02-10 00:47:38 -0600

    Getting data from the Flickr Account

--- server/app/controllers/memories.js
+++ server/app/controllers/memories.js
@@ -1,13 +1,36 @@
 var Flickr = require("flickrapi"),
 	config = require("../../config");
 
-Flickr.authenticate(config.flickrOptions, function(err, flickr) {
-	if (flickr) {
-		console.log('Connected to Flickr');
-	}
-});
-
 var getMemories = function(req, res, next) {
+
+	if (req.body && req.body.geofence) {
+
+	}
+
+
+
+	Flickr.authenticate(config.flickrOptions, function(err, flickr) {
+		if (flickr) {
+
+			console.log('Connected to Flickr');
+
+			console.log(request);
+
+			flickr.photos.geo.photosForLocation({
+				api_key: config.flickrOptions.api_key,
+				user_id: config.flickrOptions.user_id,
+				authenticated: true,
+				lat: req.body.lat,
+				lon: req.body.lng,
+				accuracy: 16
+			}, function(err, result) {
+				console.log(result);
+			});
+
+		}
+	});
+
+
 }
 
 var addMemories = function(req, res, next) {
