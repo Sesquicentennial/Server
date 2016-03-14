6be6d8736aae6198f98cf1aaaf39af0cab1a6229
israbbani <rabbanii@carleton.edu>
2016-03-14 01:23:36 -0500

    Added a connection pool for mysql.
    Modified the geofences endpoint so it uses the connection pool

--- server/app/core/database.js
+++ server/app/core/database.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql'),
-    config = require('../../config');
+    config = require('../../config'),
+    config_re = require('../../config_re');
 
 var connection = mysql.createConnection(config);
+var connectionPool = mysql.createPool(config_re.mysql);
+
 
 connection.connect();
 
 module.exports = {
-	connection: connection
+	connection: connection,
+	pool: connectionPool
 }
